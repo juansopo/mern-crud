456181d8d9516b09386e3f73bd85339bb05a2f57
Franco Franzini <francofranzini12@gmail.com>
2023-12-27 20:20:57 -0300

    agregada la tabla de socios con MUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage.jsx";
-import SociosPage from "./pages/SociosPage.jsx";
+import {Tabla} from "./pages/SociosPage.jsx";
 import SocioFormPage from "./pages/SocioFormPage.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
@@ -22,7 +22,7 @@ function App() {
               <Route path="/register" element={<RegisterPage />} />
 
               <Route element={<ProtectedRoute />}>
-                <Route path="/socios" element={<SociosPage />} />
+                <Route path="/socios" element={<Tabla />} />
                 <Route path="/add-socio" element={<SocioFormPage />} />
                 <Route path="/socios/:id" element={<SocioFormPage />} />
                 <Route path="/profile" element={<ProfilePage />} />
--- client/src/pages/SociosPage.jsx
+++ client/src/pages/SociosPage.jsx
@@ -1,41 +1,68 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
+// @ts-ignore
+//import { useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table'
+import MUIDataTable from 'mui-datatables'
+import "../input.css";
 
-import '../input.css';
-import { useSocio } from "../context/SocioContext"
-function SociosPage() {
+import { useSocio } from "../context/SocioContext";
+
+
+const columns = ["Nro Orden", "Nombre", "Email"]
+
+const options = { filterType: 'checkbox', }
+
+export const Tabla = () => {
 
     const { getAllSocios, socios } = useSocio();
 
+    useEffect(() => {
+        getAllSocios();
+    }, []);
+
+    console.log(socios)
+    const data = socios.map(socio => 
+        [socio.nroorden, socio.nombre, socio.email])
+
+
+
+    return (<MUIDataTable
+        title={"Manuel Primo"}
+        columns={columns}
+        data={data}
+        options={options}
+    />)
+}
+
+function SociosPage() {
+    const { getAllSocios, socios } = useSocio();
 
     useEffect(() => {
         getAllSocios();
-    },[])
+    }, []);
 
     return (
         <div className="bg-zinc-800 max-w-md p-10 rounded-md my-2 py-2 mx-auto">
-            <table className="w-full border">
+            <table className="table table-bordered">
                 <thead>
-                    <tr className="bg-gray-800 text-white">
+                    <tr className="bg-gray-800 text-white mx-auto">
                         <th className="border p-2">NRO ORDEN</th>
                         <th className="border p-2">NOMBRE</th>
                         <th className="border p-2">EMAIL</th>
                     </tr>
-                </thead>     
+                </thead>
                 <tbody>
-                {
-                    socios.map((socio, i) => (
+                    {socios.map((socio, i) => (
                         <tr className="bg-gray-800 " key={i}>
                             <td className="border p-2">{socio.nombre}</td>
                             <td className="border p-2">{socio.nroorden}</td>
                             <td className="border p-2">{socio.email}</td>
                         </tr>
-                    ))
-                }
+                    ))}
                 </tbody>
             </table>
         </div>
-    )
+    );
 }
 
-export default SociosPage
+export default Tabla;
